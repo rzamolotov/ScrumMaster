8b3268c5950e74e757361ca845f8e901e4dc7c87
Roman Zamolotov <rzamolotov@gmail.com>
2022-12-31 15:49:40 +0300

    Initial Commit

---
+++ ScrumMaster/ScrumMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumMasterApp.swift
+//  ScrumMaster
+//
+//  Created by Роман Замолотов on 31.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
